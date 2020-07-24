9c8faec417d7bca89e5da8005d9247cb1854d688
Guan Gui <root@guiguan.net>
2020-07-24 18:38:26 +1000

    Add overwrite option for Linux [skip ci]

--- cmd/proofable-cli/install.sh
+++ cmd/proofable-cli/install.sh
@@ -5,10 +5,13 @@ PLATFORM=$(uname)
 ENV=${ENV:="prd"}
 INSTALL_PATH=${INSTALL_PATH:="/usr/local/bin"}
 
+OPTS=""
+
 if [[ "$PLATFORM" = "Darwin" ]]; then
     DOWNLOAD_LINK="https://storage.googleapis.com/provendb-$ENV/proofable-cli/proofable-cli_darwin_amd64.tar.gz"
 elif [[ "$PLATFORM" = "Linux" ]]; then
     DOWNLOAD_LINK="https://storage.googleapis.com/provendb-$ENV/proofable-cli/proofable-cli_linux_amd64.tar.gz"
+    OPTS="--overwrite"
 else
     echo "unsupported platform \`$PLATFORM\`, please try to build from source: https://github.com/SouthbankSoftware/proofable/tree/master/cmd/proofable-cli#build-your-own-binary"
     exit 1
@@ -26,4 +29,4 @@ else
 fi
 
 mkdir -p "$INSTALL_PATH"
-eval "$DOWNLOAD_CMD" | tar -zxvC "$INSTALL_PATH" --no-same-owner
+eval "$DOWNLOAD_CMD" | tar -zxvC "$INSTALL_PATH" --no-same-owner $OPTS
